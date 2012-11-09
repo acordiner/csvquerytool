f4825e68e25318a1abf42f0a3a0a81f8d0d29945
acordiner <devnull@localhost>
2012-11-09 14:00:36 +1100

    Fixed setup URL

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from distutils.core import setup
 setup(
     name='csvquerytool',
     version='0.0.1',
-    home_page='http://bitbucket.org/acordiner/csvquerytool/',
     author='Alister Cordiner',
     author_email='alister@cordiner.net',
     packages=['csvquerytool', 'csvquerytool.test'],
     scripts=['bin/csvquery'],
-    url='http://pypi.python.org/pypi/csvquerytool/',
+    url='http://bitbucket.org/acordiner/csvquerytool/',
     license='http://www.gnu.org/licenses/gpl.html',
     description='Execute SQL queries on CSV files.',
     long_description=open('README.txt').read(),
