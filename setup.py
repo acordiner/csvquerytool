84a140957c282c420736ece161e16ca8c0c49109
acordiner <devnull@localhost>
2012-11-09 17:57:19 +1100

    Update version to match tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='csvquerytool',
-    version='0.0.1',
+    version='0.0.2',
     author='Alister Cordiner',
     author_email='alister@cordiner.net',
     packages=['csvquerytool', 'csvquerytool.test'],
