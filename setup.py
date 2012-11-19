89cbf20c5bc4c1ac05755621747b30f2c6fbbc55
acordiner <devnull@localhost>
2012-11-19 15:18:24 +1100

    Increment version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='csvquerytool',
-    version='0.0.2',
+    version='0.0.3',
     author='Alister Cordiner',
     author_email='alister@cordiner.net',
     packages=['csvquerytool', 'test'],
